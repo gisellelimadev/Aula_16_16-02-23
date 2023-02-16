84a282bdd77a9de4905bf26087c9b8322e098b0b
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-02-16 11:57:11 -0300

    aula16

---
+++ exercicios/eventos.js
@@ -0,0 +1,9 @@
+let numero = 1;
+
+while (numero <= 100) {
+    if (numero % 2 == 0)
+    document.write (numero + "<br>");
+    numero++;
+
+}
+
---
+++ musica/evantos.js
@@ -0,0 +1,19 @@
+
+let numero = 1;
+
+while (numero <= 1255) {
+
+    document.write("");
+
+    numero = numero +1;
+}
+
+document.write("<hr>");
+
+let n = 1;
+
+while (n <= 5) {
+    document.write("<br> Agr na velocidade: " + n);
+    document.write("<br> Creu creu <br>");
+    n = n + 1;
+}
